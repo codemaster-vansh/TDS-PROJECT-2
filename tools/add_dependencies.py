34c9c96597f272f90eb137b642bba8f33a9bb6af
codemaster-vansh <vanshcodes124@gmail.com>
2025-11-28 23:51:08 +0530

    Dependencies and initiation

---
+++ tools/add_dependencies.py
@@ -0,0 +1,36 @@
+from typing import List
+from langchain_core.tools import tool
+import subprocess
+
+@tool
+def add_dependencies(dependencies: List[str]) -> str:
+    """
+    Install the given python packages to the environment
+    
+    Parameters:
+        dependencies (List[str]):
+            A list of Python packages names to install
+
+    Returns:
+        str:
+            A message indicating success or failure.
+    """
+
+    try:
+        subprocess.check_call(
+            ["uv", "add"] + dependencies,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        return "Successfully installed dependencies: " + ", ".join(dependencies)
+    
+    except subprocess.CalledProcessError as e:
+        return (
+            "Dependency installation failed.\n"
+            f"Exit code: {e.returncode}\n"
+            f"Error: {e.stderr or 'No error output.'}"
+        )
+
+    except Exception as e:
+        return f"Unexpected error while installing dependencies: {e}" 
